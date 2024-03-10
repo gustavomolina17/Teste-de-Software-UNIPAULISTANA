9a0e85fe599367d46131aee8f14ef46602107848
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2024-03-10 13:04:22 -0300

    Create OperadorAtribuição.java

---
+++ TSW 2024 - Aula 04 - Operadores/OperadorAtribuição.java
@@ -0,0 +1,26 @@
+/*
+ * Professor Ms Gustavo Molina
+ * Disciplina: Teste de Software
+ * 10/03/2024
+ * 
+*/
+
+public class OperadorAtribuição{
+public static void main(String[] args) {
+    
+int numero = 10; // Atribui o valor inteiro 10 à variável 'numero'
+double valor = 3.14; // Atribui o valor de ponto flutuante 3.14 à variável 'valor'
+boolean condicao = true; // Atribui o valor booleano 'true' à variável 'condicao'
+char letra = 'A'; // Atribui o caractere 'A' à variável 'letra'
+String nome = "João"; // Atribui a cadeia de caracteres "João" à variável 'nome'
+int resultado = 2 + 3 * 4; // Atribui o valor 14 à variável 'resultado'
+
+System.out.println("Valor inteiro: "+ numero);
+System.out.println("Valor double: "+valor);
+System.out.println("Valor booleano: "+condicao);
+System.out.println("A letra digitada foi: "+letra);
+System.out.println("O nome de quem ganhou o PIX misterioso é: "+nome);
+System.out.println("O valor final da expressão é: "+resultado);
+
+}
+}
