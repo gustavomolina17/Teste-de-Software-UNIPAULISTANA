27f39d8c5a460244f6adb781fe5befd916c54f45
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2024-04-08 17:08:14 -0300

    Create TestePar.java

---
+++ TSW 2024 - Aula 07 - JUnit/Programa 01 - TestePar/TestePar.java
@@ -0,0 +1,31 @@
+
+/* Teste Manual 02 - Verifica se o número digitado pelo usuário é par
+ * 
+ * Autor: Profº Ms Gustavo Molina
+ * 01/04/2024
+ * 
+ */
+
+import java.util.Scanner;
+
+public class TestePar {
+
+    public static void main(String[] args) {
+        testarNumeroPar();
+    }
+
+    public static void testarNumeroPar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("Teste bem-sucedido: o número é par");
+        } else {
+            System.out.println("Teste falhou: o número não é par");
+        }
+        scanner.close();
+    }
+    
+}
